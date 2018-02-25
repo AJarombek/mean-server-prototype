c3b0a84154d8de56c97d79af74a2848e51e8ee27
Andrew Jarombek <ajarombek95@gmail.com>
2018-02-25 15:25:40 -0500

    7 - Create the Post Schema and Attempt TravisCI Fix

--- src/model/post.js
+++ src/model/post.js
@@ -1,2 +1,59 @@
+/**
+ * Schema for the Post object in MongoDB
+ * @author Andrew Jarombek
+ * @since 2/25/2018
+ */
 
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    id: Schema.Types.ObjectId,
+    picture: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        validate: [
+            function (desc) {
+                return desc.length < 63;
+            },
+            'Name must be less than 63 characters'
+        ]
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    first: String,
+    last: String,
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    description: {
+        type: String,
+        validate: [
+            function (desc) {
+                return desc.length < 255;
+            },
+            'Description must be less than 255 characters'
+        ]
+    },
+    up: {
+        type: Number,
+        default: 0
+    },
+    down: {
+        type: Number,
+        default: 0
+    }
+});
+
+PostSchema.index({username: 1});
+
+module.exports = mongoose.model('Post', PostSchema, 'post');
